6bc105fad677ed8a1d529f48c7326f79711e9585
NhatThai <nhat.thai@dfinsolutions.com>
2021-11-28 13:45:27 +0700

    Add package.json and data

---
+++ shared-element-transitions/config/data.js
@@ -0,0 +1,29 @@
+export const data = [
+  {
+    id: '1',
+    title: 'Manarola, Italy',
+    description: 'The Cliffs of Cinque Terre',
+    image_url:
+      'https://images.unsplash.com/photo-1516483638261-f4dbaf036963?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80',
+    iconName: 'location-pin',
+    content: 'Manarola may be the oldest of the towns in the Cinque Terre, with the cornerstone of the church, San Lorenzo, dating from 1338. The local dialect is Manarolese, which is marginally different from the dialects in the nearby area. The name "Manarola" is probably a dialectical evolution of the Latin, "magna rota". In the Manarolese dialect this was changed to "magna roea" which means "large wheel", in reference to the mill wheel in the town. \r\nManarola\'s primary industries have traditionally been fishing and wine-making. The local wine, called Sciacchetrà, is especially renowned; references from Roman writings mention the high quality of the wine produced in the region. In recent years, Manarola and its neighboring towns have become popular tourist destinations, particularly in the summer months. Tourist attractions in the region include a famous walking trail between Manarola and Riomaggiore (called Via dell\'Amore, "Love\'s Trail") and hiking trails in the hills and vineyards above the town. Manarola is one of the five villages of the Cinque Terre. Most of the houses are bright and colourful'
+  },
+  {
+    id: '2',
+    title: 'Venezia, Italy',
+    description: 'Rialto Bridge, Venezia, Italy',
+    image_url:
+      'https://images.unsplash.com/photo-1523906834658-6e24ef2386f9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=630&q=80',
+    iconName: 'location-pin',
+    content: 'Venice, the capital of northern Italy\’s Veneto region, is built on more than 100 small islands in a lagoon in the Adriatic Sea. It has no roads, just canals – including the Grand Canal thoroughfare – lined with Renaissance and Gothic palaces. \r\nThe central square, Piazza San Marco, contains St. Mark’s Basilica, which is tiled with Byzantine mosaics, and the Campanile bell tower offering views of the city’s red roofs'
+  },
+  {
+    id: '3',
+    title: 'Prague, Czechia',
+    description: 'Tram in Prague',
+    image_url:
+      'https://images.unsplash.com/photo-1513805959324-96eb66ca8713?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    iconName: 'location-pin',
+    content: 'Prague, capital city of the Czech Republic, is bisected by the Vltava River. Nicknamed \“the City of a Hundred Spires,\” it\'s known for its Old Town Square, the heart of its historic core, with colorful baroque buildings, Gothic churches and the medieval Astronomical Clock, which gives an animated hourly show. \r\nCompleted in 1402, pedestrian Charles Bridge is lined with statues of Catholic saints'
+  }
+];
